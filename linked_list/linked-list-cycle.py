24389c6f8aeb870944bd1ff7815bc1aa67243a1c
Leuel Asfaw <105054376+leuel-a@users.noreply.github.com>
2023-11-21 21:22:40 +0300

    Added solution for Linked List Cycle

---
+++ linked_list/linked-list-cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        tortoise = haire = head
+
+        while haire:
+            haire = haire.next
+
+            if haire:
+                tortoise = tortoise.next
+                haire = haire.next
+
+            if haire is tortoise:
+                return True
+        return False
